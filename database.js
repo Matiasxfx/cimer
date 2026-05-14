283c68547c012e3c606e7d0270385c4c40b45660
Matiasxfx <168347396+Matiasxfx@users.noreply.github.com>
2026-05-14 11:24:50 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,87 @@
+const paginasManuales = [
+       {
+        titulo: "MAS VENDIDOS",
+        productos: [
+            { sku: "R567402", nombre: "KG. CAÑO 1/4 ROLLO DE COBRE" },        
+            { sku: "R567406", nombre: "KG. CAÑO 3/8 ROLLO DE COBRE" },
+            { sku: "R567408", nombre: "KG. CAÑO 1/2 ROLLO DE COBRE" },
+            { sku: "R969011", nombre: "AIS NEGRA 1/4 TIRA 6MM" },
+            { sku: "R969012", nombre: "AIS NEGRA 3/8 TIRA 6MM" },
+            { sku: "R969013", nombre: "AIS NEGRA 1/2 TIRA 6MM" },
+            { sku: "R782070", nombre: "CABLES 5 X 1.5" },
+            { sku: "R767802", nombre: "MANGUERA CRISTAL X MT 14X18"   },
+            { sku: "R124545", nombre: "CINTA S/ PEGAMENTO BLANCA" }
+           
+        ]
+    },
+   
+     
+   
+   
+   
+   
+   
+    {
+        titulo: "CAÑERÍA DE COBRE",
+        productos: [
+            { sku: "R567402", nombre: "KG. CAÑO 1/4 ROLLO DE COBRE" },
+            { sku: "R567404", nombre: "KG. CAÑO 5/16 ROLLO DE COBRE" },
+            { sku: "R567406", nombre: "KG. CAÑO 3/8 ROLLO DE COBRE" },
+            { sku: "R567408", nombre: "KG. CAÑO 1/2 ROLLO DE COBRE" },
+            { sku: "R567410", nombre: "KG. CAÑO 5/8 ROLLO DE COBRE" },
+            { sku: "R567412", nombre: "KG. CAÑO 3/4 ROLLO DE COBRE" },
+            { sku: "R567414", nombre: "KG. CAÑO 7/8 ROLLO DE COBRE" }
+        ]
+    },
+    {
+        titulo: "AISLACIONES NEGRAS",
+        productos: [
+            { sku: "R969011", nombre: "AIS NEGRA 1/4 TIRA 6MM" },
+            { sku: "R969012", nombre: "AIS NEGRA 3/8 TIRA 6MM" },
+            { sku: "R969013", nombre: "AIS NEGRA 1/2 TIRA 6MM" },
+            { sku: "R969014", nombre: "AIS NEGRA 5/8 TIRA 6MM" },
+            { sku: "R969015", nombre: "AIS NEGRA 3/4 TIRA 6MM" },
+            { sku: "R969016", nombre: "AIS. NEGRA 7/8 TIRA 9MM" },
+            { sku: "R969020", nombre: "AIS NEGRA 5/8 TIRA 9MM" }
+        ]
+    },
+    {
+        titulo: "GASES REFRIGERANTES X KG",
+        productos: [
+            { sku: "R185010", nombre: "KG. FREON R 141 B" },
+            { sku: "R185011", nombre: "KG HY-12 RE" },
+            { sku: "R185014", nombre: "KG R22 PURO" },
+            { sku: "R185017", nombre: "KG R-134" },
+            { sku: "R185018", nombre: "KG R-404" },
+            { sku: "R185023", nombre: "KG R-407" },
+            { sku: "R185024", nombre: "KG R-410" },
+            { sku: "R185027", nombre: "KG R-22 MEZCLA" }
+        ]
+    },
+    {
+        titulo: "CABLES POR METRO",
+        productos: [
+            { sku: "R782044", nombre: "CABLE 2 X 1.5" },
+            { sku: "R782045", nombre: "CABLE 2 X 2.5" },
+            { sku: "R782047", nombre: "CABLES 3 X 1.5" },
+            { sku: "R782050", nombre: "CABLES 3 X 2.5" },
+            { sku: "R782054", nombre: "CABLES 4 X 1.5" },
+            { sku: "R782056", nombre: "CABLES 4 X 2.5" },
+            { sku: "R782070", nombre: "CABLES 5 X 1.5" },
+            { sku: "R782080", nombre: "CABLES 5 X 2.5" },
+            { sku: "R782082", nombre: "CABLES 7 X 1.5" }
+        ]
+    },
+    {
+        titulo: "ACCESORIOS INSTALACIÓN",
+        productos: [
+            
+            { sku: "R767801", nombre: "MANGUERA CRISTAL X MT 12X16" },
+            { sku: "R767802", nombre: "MANGUERA CRISTAL X MT 14X18"   },
+            { sku: "R488110", nombre: "100 PRECINTOS 20 CM NEGROS" },
+            { sku: "R488103", nombre: "100 PRECINTOS 200 MM BLANCOS" },
+            { sku: "R146125", nombre: "100 TACOS LOCK BLANCOS" },
+            { sku: "R124545", nombre: "CINTA S/ PEGAMENTO BLANCA" }
+        ]
+    }
+];
